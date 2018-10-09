a89541ce0852b4ab611d77fa3bb1a241d2d96451
Valentina Versari <versavale@gmail.com>
2018-10-09 06:26:02 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+    <h1>Hello World!</h1>
+    )
+  }
+}
+
+const users = [{ name : 'Anna', age : 32}, { name : 'Kathy', age : 44 }, { name : 'Katy', age : 4 }]
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <HelloWorld/>
+          {users.map((user, i) => (<h1 key={i} >{user.name}</h1>))}
         </header>
       </div>
     );
